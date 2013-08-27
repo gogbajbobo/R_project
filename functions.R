7e28a44bcbdb055473bafa9a805678cef763f191
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-08-27 14:10:32 +0400

    add switch

--- functions.R
+++ functions.R
@@ -23,3 +23,78 @@ libraryCheck <- function(libraryList = NULL) {
 	}
 		
 }
+
+xcomSingleEnergyData <- function(Energy, Matter, select, returnValue) {
+	
+	
+	# Energy <- 1.23 #KeV
+	# ElemName <- 'Cu'
+	# Compound <- 'KBr'
+	# Mixture <- paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A') #ElementName+FractionByWeight
+	# select <- 'mixture' #element, compound or mixture
+	
+	if (select=='element') {
+		path <- '/cgi-bin/Xcom/xcom3_1-t'
+		dataToSend <- paste('ZSym=',Matter,'&Energies=',as.character(Energy/1000),'&OutOpt=PIC',sep='')
+	} else if (select=='compound') {
+		path <- '/cgi-bin/Xcom/xcom3_2-t'
+		dataToSend <- paste('Formula=',Matter,'&Energies=',as.character(Energy/1000),sep='')
+	} else if (select=='mixture') {
+		path <- '/cgi-bin/Xcom/xcom3_3-t'
+		dataToSend <- paste('Formulae=',Matter,'&Energies=',as.character(Energy/1000),sep='')
+	}
+	
+	value <- simplePostToHost('physics.nist.gov',path,'www.iptm.ru', dataToSend)
+	print(value)
+	value <- strsplit(value,'(cm2/g)\n\n       ',fixed=TRUE)
+	value <- value[[1]][2]
+	value <- strsplit(value,'\n</pre>\n',fixed=TRUE)
+	value <- value[[1]][1]
+	value <- strsplit(value,' ',fixed=TRUE)
+	
+	print(returnValue)
+	
+	switch(returnValue,
+	
+		CS = {
+			#CoherentScattering
+			return(value[[1]][2])
+		},
+
+		IS = {
+			#IncoherentScattering
+			return(value[[1]][3])
+		},
+	
+		PEA = {
+			#PhotoElectricAbsorption
+			return(value[[1]][4])
+		},
+
+		PPNF = {
+			#PairProductionInNuclearField
+			return(value[[1]][5])
+		},
+	
+		PPEF = {
+			#PairProductionInElectronField
+			return(value[[1]][6])
+		},
+
+		AWCS = {
+			#AttenuationWithCoherentScattering
+			return(value[[1]][7])
+		},
+
+		AWOCS = {
+			#AttenuationWithOutCoherentScattering
+			return(value[[1]][8])
+		},
+		
+		{
+			return(FALSE)
+		}
+	
+	)
+	
+}
--- xcom2.R
+++ xcom2.R
@@ -3,26 +3,7 @@ source('Developer/R_project/functions.R')
 libraryList <- c('XML', 'httpRequest')
 
 if (libraryCheck(libraryList)) {
-	
-	Energy <- 1.23 #KeV
-	ElemName <- 'Cu'
-	Compound <- 'KBr'
-	Mixture <- paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A') #ElementName+FractionByWeight
-	
-	select <- 'mixture' #element, compound or mixture
-	
-	if (select=='element') {
-		path <- '/cgi-bin/Xcom/xcom3_1-t'
-		dataToSend <- paste('ZSym=',ElemName,'&Energies=',as.character(Energy/1000),'&OutOpt=PIC',sep='')
-	} else if (select=='compound') {
-		path <- '/cgi-bin/Xcom/xcom3_2-t'
-		dataToSend <- paste('Formula=',Compound,'&Energies=',as.character(Energy/1000),sep='')
-	} else if (select=='mixture') {
-		path <- '/cgi-bin/Xcom/xcom3_3-t'
-		dataToSend <- paste('Formulae=',Mixture,'&Energies=',as.character(Energy/1000),sep='')
-	}
 
-	value <- simplePostToHost('physics.nist.gov',path,'www.iptm.ru', dataToSend)
-
+	xcomSingleEnergyData(1.23, 'Cu', 'element', 'IS')	
 	
 }
