70725ce264fa3eba068e1212ebb504b5c4d115fc
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2013-08-29 16:22:23 +0400

    add linear interpolation

--- functions.R
+++ functions.R
@@ -24,7 +24,7 @@ libraryCheck <- function(libraryList = NULL) {
 		
 }
 
-xcomSingleEnergyData <- function(Energy, Matter, select, returnValue) {
+xcomSingleEnergyData <- function(Energy, Matter, select, returnValueType) {
 	
 	
 	# Energy <- 1.23 #KeV
@@ -54,7 +54,7 @@ xcomSingleEnergyData <- function(Energy, Matter, select, returnValue) {
 	value <- value[[1]][1]
 	value <- strsplit(value,' ',fixed=TRUE)
 	
-	# print(returnValue)
+	# print(returnValueType)
 	
 	valueTypes <- c('CS', 'IS', 'PEA', 'PPNF', 'PPEF', 'AWCS', 'AWOCS')
 		# CS = CoherentScattering
@@ -65,7 +65,90 @@ xcomSingleEnergyData <- function(Energy, Matter, select, returnValue) {
 		# AWCS = AttenuationWithCoherentScattering
 		# AWOCS = AttenuationWithOutCoherentScattering
 
-	index <- match(returnValue, valueTypes) + 1
-	return(value[[1]][index])
+	index <- match(returnValueType, valueTypes) + 1
+	return(as.numeric(value[[1]][index]))
 		
+}
+
+
+xcomEnergyData <- function(Energy, Matter, select, returnValueType, ipType) {
+	
+	
+	# Energy <- 1.23 #KeV
+	# ElemName <- 'Cu'
+	# Compound <- 'KBr'
+	# Mixture <- paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A') #ElementName+FractionByWeight
+	# select <- 'mixture' #element, compound or mixture
+	
+	parameters <- '&OutOpt=PIC&Graph0=on&NumAdd=1&Output=on&WindowXmin=0.001&WindowXmax=0.1&ResizeFlag=on'
+	if (select=='element') {
+		path <- '/cgi-bin/Xcom/xcom3_1'
+		dataToSend <- paste('ZSym=', Matter, parameters, sep='')
+	} else if (select=='compound') {
+		path <- '/cgi-bin/Xcom/xcom3_2'
+		dataToSend <- paste('Formula=', Matter, parameters, sep='')
+	} else if (select=='mixture') {
+		path <- '/cgi-bin/Xcom/xcom3_3'
+		dataToSend <- paste('Formulae=', Matter, parameters ,sep='')
+	}
+	
+	response <- simplePostToHost('physics.nist.gov',path,'www.iptm.ru', dataToSend)
+	response <- strsplit(response, '<html>', fixed=TRUE)
+	# print(response)
+	response <- response[[1]][2]
+	responseHtml <- htmlParse(response, asText=TRUE)
+	table <- getNodeSet(responseHtml, '//table')
+	tr <- getNodeSet(table[[1]], '//tr')
+	
+	for (i in 5:length(tr)) {
+		td1 <- getNodeSet(tr[[i-1]], './td')
+		en1 <- as.numeric(xmlValue(td1[[2]])) * 1000
+
+		td2 <- getNodeSet(tr[[i]], './td')
+		en2 <- as.numeric(xmlValue(td2[[2]])) * 1000
+		
+		if (Energy >= en1 && Energy < en2) {
+			
+			# print(paste(Energy, 'beetwen', en1, 'and', en2))
+			value <- ipData(td1, td2, Energy, returnValueType, ipType)
+			return(value)
+			break
+			
+		}
+
+	}
+		
+}
+
+
+ipData <- function(lowValues, highValues, Energy, returnValueType, ipType) {
+	
+	lowEnergy <- as.numeric(xmlValue(lowValues[[2]])) * 1000
+	highEnergy <- as.numeric(xmlValue(highValues[[2]])) * 1000
+	
+	valueTypes <- c('CS', 'IS', 'PEA', 'PPNF', 'PPEF', 'AWCS', 'AWOCS')
+	index <- match(returnValueType, valueTypes) + 2
+	lowValue <- as.numeric(xmlValue(lowValues[[index]]))
+	highValue <- as.numeric(xmlValue(highValues[[index]]))
+
+	if (ipType == 'linear') {
+		
+		returnValue <- linearIp(Energy, lowEnergy, highEnergy, lowValue, highValue)
+		return(returnValue)
+		
+	} else {
+		
+		return()
+		
+	}	
+
+}
+
+
+linearIp <- function(x, x1, x2, y1, y2) {
+	
+	# print(paste(c('linearIp', x, x1, x2, y1, y2)))
+	y <- y1 + ((y2 - y1) / (x2 - x1)) * (x - x1)
+	return(y)
+	
 }
--- xcom2.R
+++ xcom2.R
@@ -4,8 +4,15 @@ libraryList <- c('XML', 'httpRequest')
 
 if (libraryCheck(libraryList)) {
 
-	response <- xcomSingleEnergyData(1.23, 'Cu', 'element', 'IS')	
-	
+	response <- xcomSingleEnergyData(1.23, 'Cu', 'element', 'AWCS')	
+	# response <- xcomSingleEnergyData(1.23, 'KBr', 'compound', 'AWCS')	
+	# response <- xcomSingleEnergyData(1.23, paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A'), 'mixture', 'AWCS')
+	print(response)
+
+	response <- xcomEnergyData(1.23, 'Cu', 'element', 'AWCS', 'linear')	
+	# response <- xcomEnergyData(1.23, 'KBr', 'compound', 'AWCS', 'linear')
+	# response <- xcomEnergyData(1.23, paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A'), 'mixture', 'AWCS', 'linear')
+
 	print(response)
 	
 }
