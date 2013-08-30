f1312c2de9d1c906787824b819a011ebe1f67e7b
Rashid <tangreez@gmail.com>
2013-08-30 19:34:46 +0400

    interaction emix-mode with data.env

--- xcom2.R
+++ xcom2.R
@@ -38,7 +38,8 @@ if (libraryCheck(libraryList)) {
 	
 	# вызов функции
 	
-	response <- xcomEnergyData(1.23, 'Cu', 'element', 'AWCS', 'linear')	
+# 	response <- xcomEnergyData(1.23, 'Cu', 'element', 'AWCS', 'linear')
+	response <- xcomEnergyData(xlines['K'], est_mix, 'mixture', 'AWCS', 'linear')
 	# response <- xcomEnergyData(1.23, 'KBr', 'compound', 'AWCS', 'linear')
 	# response <- xcomEnergyData(1.23, paste('Cu+0.7','Zn+0.25','NaCl+0.05',sep='%0D%0A'), 'mixture', 'AWCS', 'linear')
 
