54f5bf021443e7eab342bde7d9229c96907de2c7
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-01-08 16:59:18 +0400

    show graph

--- accelData.R
+++ accelData.R
@@ -1,9 +1,10 @@
 file_name <- '/tmp/accel/1389183243_9586.acceldata'
 
-accelData <- read.table(file_name,,dec='.',sep='',header=FALSE,skip=2)
+accelData <- read.table(file_name,,dec='.',sep='',header=TRUE)
 
-print(n)
+# print(accelData)
 
-plot(1:l,n[,2],type='l',xlab='',ylab='',log='y',ylim=c(min(n[,2:4]),max(n[,2:4])))
-lines(1:l,n[,3],type='l',col='red')
-lines(1:l,n[,4],type='l',col='blue')
+data <- accelData[,1]
+n <- length(data)
+
+plot(1:n, data, type = 'l')
