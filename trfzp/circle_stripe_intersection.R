c93e3406b17260664a5b4effcdcb82813872a274
Grigoriev Maxim <grigoriev.maxim@gmail.com>
2014-03-21 17:16:17 +0400

    adjust parameters

--- trfzp/circle_stripe_intersection.R
+++ trfzp/circle_stripe_intersection.R
@@ -1,11 +1,23 @@
 R1 <- 24 # diafragm radius, µm
-R2 <- 74 # ring radius, µm, should be more than R1
+R2 <- 157 # ring radius, µm, should be more than R1
 D <- 100 # stripe width, µm
-step <- 1 # µm
-N <- 1 # normalize coefficient
+step <- 1 # µm — get from scan step
+N <- 970 # normalize coefficient
 
 T1 <- 0 # inner thickness, µm
 T2 <- 100 # outer thickness, µm
+
+# sm
+# diff 	95360.41
+# R1	24
+# R2 	157
+# N		970
+
+# smooth(sm)
+# diff 	42660.14
+# R1	24
+# R2 	157
+# N		971
 
 Ring_Thickness <- function(r) {
 
@@ -15,7 +27,7 @@ Ring_Thickness <- function(r) {
 
 	} else if (r > R2) {
 
-		t <- T2
+		t <- Inf
 
 	} else {
 
@@ -67,14 +79,16 @@ for (i in ring_range) {
 ring <- step^2 * exp(ring_t * log(10^-4) / T2) # 10^-4 — transmition for 100µm Mo at 9.7keV
 ring[is.na(ring)] <- 0
 
-intersection <- array()
-
 range <- seq(-(R2+D/2), R2+D/2, step)
+intersection <- array(NA, length(range))
+dimnames(intersection) <- list(range)
 
 for (i in range) {
 
 	edge1 <- i + D/2
 	edge2 <- i - D/2
+
+	# print(c(edge1, edge2))
 
 	I1 <- S_Calc(edge1, R1) - S_Calc(edge2, R1)
 
@@ -84,11 +98,40 @@ for (i in range) {
 
 }
 
-intersection <- intersection[!is.na(intersection)]
+# intersection <- intersection[!is.na(intersection)]
+intersection <- N * intersection / max(intersection)
 
-plot(range, intersection, xlab = paste('µm,', -(R2+D/2), ':', R2+D/2), ylab = 'intensity', log = '')
+sm <- array(0, length(range))
+shift <- floor((dim(scan_map)[1] - length(range)) / 2) - 6 # scan_map from scan_map.R // 6 — hand correction
+
+for (i in seq(1, 2*R2+D, 1)) {
+
+	if (i+shift <= 0 || i+shift > dim(scan_map)[1]) {
+		sm[i] <- 0
+	} else {
+		sm[i] <- scan_map[i+shift,1]		
+	}
+
+}
+
+plot(range, sm, type = 'l', xlab = paste('µm,', -(R2+D/2), ':', R2+D/2), ylab = 'intensity', log = '', col = gray(0.8))
+lines(range, smooth(sm), col = 'blue')
+lines(range, intersection, col = 'black')
+# abline(v = which(range == -R1), col = 'red')
+# abline(v = which(range == R1), col = 'red')
+# abline(v = which(range == -R2), col='green')
+# abline(v = which(range == R2), col='green')
+
+# plot(range, intersection, xlab = paste('µm,', -(R2+D/2), ':', R2+D/2), ylab = 'intensity', log = '')
 abline(v = -R1, col = 'red')
 abline(v = R1, col = 'red')
-abline(v = -R2, col='blue')
-abline(v = R2, col='blue')
+abline(v = -R2, col='green')
+abline(v = R2, col='green')
+
+sum_range <- seq(1-shift, dim(scan_map)[1]-shift, 1)
+
+print(sum((intersection[sum_range] - smooth(sm[sum_range]))^2))
+print(sum((intersection[sum_range] - sm[sum_range])^2))
+print(c(N, R2))
+
 
